ab6e52907e8d4556ff6c412eda373232fb69cb52
Paula, Janaina P <janaina.p.paula@accenture.com>
2019-08-29 21:17:55 -0300

    Primeiro commit

---
+++ src/main/java/br/com/janadev/ecommerce/EcommerceMonolitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.janadev.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMonolitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMonolitoApplication.class, args);
+    }
+
+}
