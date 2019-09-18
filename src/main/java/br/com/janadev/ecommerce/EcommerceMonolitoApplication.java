2a72a7b1ed78db7bc1ddbdd6809ba498f5411671
Paula, Janaina P <janaina.p.paula@accenture.com>
2019-09-17 21:34:27 -0300

    Criando instancias para teste.

--- src/main/java/br/com/janadev/ecommerce/EcommerceMonolitoApplication.java
+++ src/main/java/br/com/janadev/ecommerce/EcommerceMonolitoApplication.java
@@ -1,13 +1,48 @@
 package br.com.janadev.ecommerce;
 
+import br.com.janadev.ecommerce.domain.Categoria;
+import br.com.janadev.ecommerce.domain.Produto;
+import br.com.janadev.ecommerce.repositories.CategoriaRepository;
+import br.com.janadev.ecommerce.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class EcommerceMonolitoApplication {
+public class EcommerceMonolitoApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceMonolitoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.0);
+        Produto p2 = new Produto(null, "Impressora", 800.0);
+        Produto p3 = new Produto(null, "Mouse", 80.0);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    }
 }
