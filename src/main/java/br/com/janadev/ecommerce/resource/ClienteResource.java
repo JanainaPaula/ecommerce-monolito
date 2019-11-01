3a32b11d6797367d387c2c4f74cec32cbc704350
Paula, Janaina P <janaina.p.paula@accenture.com>
2019-11-01 18:20:44 -0300

    Criando ClienteDTO e endpoints PUT, DELETE e GET para Cliente.

--- src/main/java/br/com/janadev/ecommerce/resource/ClienteResource.java
+++ src/main/java/br/com/janadev/ecommerce/resource/ClienteResource.java
@@ -1,15 +1,17 @@
 package br.com.janadev.ecommerce.resource;
 
-import br.com.janadev.ecommerce.domain.Categoria;
 import br.com.janadev.ecommerce.domain.Cliente;
-import br.com.janadev.ecommerce.services.CategoriaService;
+import br.com.janadev.ecommerce.dto.ClienteDTO;
 import br.com.janadev.ecommerce.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -19,9 +21,47 @@ public class ClienteResource {
     private ClienteService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> buscarCategoriaPorId(@PathVariable Integer id){
-        Cliente cliente = service.buscaCategoriaPorId(id);
+    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Integer id){
+        Cliente cliente = service.buscaClientePorId(id);
         return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insereCliente(@Valid @RequestBody ClienteDTO clienteDto){
+        Cliente categoriaCriada = service.insereCliente(clienteDto.fromDTO());
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(categoriaCriada.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizaCliente(@Valid @RequestBody ClienteDTO clienteDto, @PathVariable Integer id){
+        clienteDto.setId(id);
+        service.atualizaCliente(clienteDto.fromDTO());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletaCliente(@PathVariable Integer id){
+        service.deletaCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> listaCliente(){
+        List<ClienteDTO> categorias = service.buscaTodasClientes();
+        return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<ClienteDTO>> buscaPaginaCliente(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "24") Integer size,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy){
+        Page<Cliente> paginaCliente = service.buscaPaginaClientes(page, size, direction, orderBy);
+        Page<ClienteDTO> paginaClientesDTO = paginaCliente.map(ClienteDTO::new);
+        return ResponseEntity.ok(paginaClientesDTO);
     }
 
 }
--- src/main/java/br/com/janadev/ecommerce/services/ClienteService.java
+++ src/main/java/br/com/janadev/ecommerce/services/ClienteService.java
@@ -1,14 +1,20 @@
 package br.com.janadev.ecommerce.services;
 
-import br.com.janadev.ecommerce.domain.Categoria;
 import br.com.janadev.ecommerce.domain.Cliente;
+import br.com.janadev.ecommerce.dto.ClienteDTO;
+import br.com.janadev.ecommerce.exception.DataIntegrityException;
 import br.com.janadev.ecommerce.exception.ObjectNotFoundException;
-import br.com.janadev.ecommerce.repositories.CategoriaRepository;
 import br.com.janadev.ecommerce.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ClienteService {
@@ -16,9 +22,44 @@ public class ClienteService {
     @Autowired
     private ClienteRepository repository;
 
-    public Cliente buscaCategoriaPorId(Integer id){
+    public Cliente buscaClientePorId(Integer id){
         Optional<Cliente> clienteBuscado = repository.findById(id);
         return clienteBuscado.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " +
                 id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+    public Cliente insereCliente(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+
+    public Cliente atualizaCliente(Cliente cliente) {
+        Cliente clienteBuscado = buscaClientePorId(cliente.getId());
+        updateData(clienteBuscado, cliente);
+        return repository.save(clienteBuscado);
+    }
+
+    private void updateData(Cliente clienteBuscado, Cliente clienteAtualizado) {
+        clienteBuscado.setNome(clienteAtualizado.getNome());
+        clienteBuscado.setEmail(clienteAtualizado.getEmail());
+    }
+
+    public void deletaCliente(Integer id) {
+        buscaClientePorId(id);
+        try {
+            repository.deleteById(id);
+        }catch (DataIntegrityViolationException ex){
+            throw new DataIntegrityException("Não foi possível deletar pois há entidades relacionadas.");
+        }
+
+    }
+
+    public List<ClienteDTO> buscaTodasClientes() {
+        return repository.findAll().stream().map(ClienteDTO::new).collect(Collectors.toList());
+    }
+
+    public Page<Cliente> buscaPaginaClientes(Integer page, Integer size, String direction, String orderBy){
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
     }
 }
