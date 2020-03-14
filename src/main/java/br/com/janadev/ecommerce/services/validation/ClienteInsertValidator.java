bfe5b68d8b04b06b99228fd915998e4dfe504511
Janaina Paula <janainadepaula93@gmail.com>
2020-03-14 18:24:48 -0300

    Adicionando validacao para email repetido

--- src/main/java/br/com/janadev/ecommerce/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/janadev/ecommerce/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package br.com.janadev.ecommerce.services.validation;
 
+import br.com.janadev.ecommerce.domain.Cliente;
 import br.com.janadev.ecommerce.dto.ClienteNewDTO;
 import br.com.janadev.ecommerce.enums.TipoCliente;
 import br.com.janadev.ecommerce.handler.FieldMessage;
+import br.com.janadev.ecommerce.repositories.ClienteRepository;
 import br.com.janadev.ecommerce.services.validation.util.BRDocumentUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repository;
 
     @Override
     public void initialize(ClienteInsert constraintAnnotation) {
@@ -29,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if (clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) &&
                 !BRDocumentUtil.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())){
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente clienteExistente = repository.findByEmail(clienteNewDTO.getEmail());
+        if (clienteExistente != null){
+            list.add(new FieldMessage("email", "Esse email já existe."));
         }
 
         for (FieldMessage fieldMessage : list){
