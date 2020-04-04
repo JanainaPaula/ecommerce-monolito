7ddf4e7ff2468ac0d1843ffe8b90e48c408be113
Janaina Paula <janainadepaula93@gmail.com>
2020-04-04 17:11:33 -0300

    Criando profile de desenvolvimento

--- src/main/java/br/com/janadev/ecommerce/config/DevConfig.java
+++ src/main/java/br/com/janadev/ecommerce/config/DevConfig.java
@@ -2,6 +2,7 @@ package br.com.janadev.ecommerce.config;
 
 import br.com.janadev.ecommerce.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,19 +10,25 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     private final DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Autowired
-    public TestConfig(DBService dbService) {
+    public DevConfig(DBService dbService) {
         this.dbService = dbService;
     }
 
     @Bean
     public boolean initialiazeDatabase() throws ParseException {
-        dbService.initializeDatabase();
-        return true;
+        if (strategy.equals("create")){
+            dbService.initializeDatabase();
+            return true;
+        }
+        return false;
     }
 }
