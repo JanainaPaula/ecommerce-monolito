70222ce82718edca94f17fb24c0a5104e3c60a46
Janaina Paula <janainadepaula93@gmail.com>
2020-05-03 14:02:31 -0300

    Salvando url da imagem no model Cliente.

--- src/main/java/br/com/janadev/ecommerce/domain/Cliente.java
+++ src/main/java/br/com/janadev/ecommerce/domain/Cliente.java
@@ -25,6 +25,8 @@ public class Cliente implements Serializable {
     @JsonIgnore
     private String senha;
 
+    private String imageUrl;
+
     @OneToMany(mappedBy = "cliente", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
@@ -100,6 +102,14 @@ public class Cliente implements Serializable {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public List<Endereco> getEnderecos() {
